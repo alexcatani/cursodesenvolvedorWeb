789a95a99ca067007e9776c32a3760666e0e94b4
Alex Catani <40073083+alexcatani@users.noreply.github.com>
2019-12-15 12:46:06 -0300

    201912142330

---
+++ Cursos/Dev_WEB-Udemy/fundamentos/organizacao.js
@@ -0,0 +1,14 @@
+console.log(
+    "Sentença de código")
+
+{  // Blocos de códigos
+    {
+        console.log("Olá");
+        console.log("Mundo!") // Padrão para o curso
+    }
+}
+
+// Funções, classe: tem blocos associados.
+// If: pode ou não ter blocos associados.
+
+// Java Scrip é organizado em sentenças de codigos (que podem terminar com ponto e virgula) e blocos de codigo.
---
+++ Cursos/Dev_WEB-Udemy/fundamentos/var_let_const.js
@@ -0,0 +1,11 @@
+var a = 3   // var: é uma palavra reservada
+            // a: indentificador que dado manualmente
+            // = 3: atribuindo um valor.
+
+let b = 4   // Existe difenteça entre var e let.
+
+
+var = 30
+let = 40
+console.log(a, b)
+
