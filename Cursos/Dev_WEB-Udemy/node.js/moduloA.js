cc1112ac21a73f4bbdfbd71107138d761cf7bb51
Alex Catani <af.catani@gmail.com>
2021-01-11 20:35:55 -0300

    aula125-Node
    Visão geral sobre o Node.js

---
+++ Cursos/Dev_WEB-Udemy/node.js/moduloA.js
@@ -0,0 +1,10 @@
+// Aplicações back-end em Node são organizadas em Pastas e Arquivos
+
+// Em Node um arquivo representa um modulo
+// Funções devem ser exportadas para ficar visizer paro o restante da aplicaçao
+
+this.ola = 'Fala Pessoal'
+
+exports.bemVindo = 'Bem vindo ao node!'
+
+module.esports.atelogo = 'Até a porximo exemplo'
